055eb938c12caf1c74332abc9ca1ec2151fa0995
rebelben99 <rebelben99@gmail.com>
2024-03-09 06:03:34 -0500

    Update GameData.h

--- version/GameData.h
+++ version/GameData.h
@@ -3,12 +3,12 @@ class GameData
 {
 	public:
 		bool	InfAmmo = false;
-		bool	InfAmmoLegit = false;
+		bool	InfAmmoLegit = true;
 		bool	InfGrenades = false;
-		bool	InfGrenadesLegit = false;
+		bool	InfGrenadesLegit = true;
 		bool	InfSyringes = false;
-		bool	InfSyringesLegit = false;
-		bool	InfStratagems = false;
+		bool	InfSyringesLegit = true;
+		bool	InfStratagems = true;
 		bool	InfMissionTime = false;
 		bool	InfStamina = false;
 		bool	InfHealth = false;
@@ -23,11 +23,11 @@ class GameData
 		bool	NoStasTurretOverHeat = false;
 		bool	JetpackNoCD = false;
 		bool	ShieldNoCD = false;
-		bool	InstantRailGun = false;
+		bool	InstantRailGun = true;
 		bool	ShowAllMapIcons = false;
-		bool	AllStratagems = false;
-		bool	AllEquipment = false;
-		bool	AllArmor = false;
+		bool	AllStratagems = true;
+		bool	AllEquipment = true;
+		bool	AllArmor = true;
 		bool	Speedhack = false;
 
 		bool	OHK = false;
